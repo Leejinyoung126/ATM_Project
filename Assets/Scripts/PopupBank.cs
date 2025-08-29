ad02a2720b1428bc1440266ee9e2fab2a4fe4ed0
Leejinyoung126 <shinyshines76@gmail.com>
2025-08-29 10:43:23 +0900

    Feat/스텝8 입금기능과 스텝9 출금기능 제작

--- Assets/Scripts/PopupBank.cs
+++ Assets/Scripts/PopupBank.cs
@@ -1,13 +1,24 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class PopupBank : MonoBehaviour
 {
+    //필드에 캐싱
+    private UserData d;
+
     public GameObject ButtonGroup;
     public GameObject DepositUI;
     public GameObject WithdrawalUI;
-    
+    public GameObject PopupError;
+    public MoneyNum moneyNum;
+    public InputField depositInputField;
+    public InputField withdrawalInputField;
+
+    private void Start()
+    {
+        d = GameManager.Instance.userData;
+    }
+
     public void OpenDeposit()
     {
         DepositUI.SetActive(true);
@@ -29,13 +40,72 @@ public class PopupBank : MonoBehaviour
         ButtonGroup.SetActive(true);
     }
 
-    public void Deposit()
+    public void Deposit(int amount)
     {
+        
+        //현금이 부족하면 팝업에러창을 띄움
+        if(d.cash < amount)
+        {
+            PopupError.SetActive(true);
+            return;
+        }
 
+        //입금처리
+        d.cash -= amount;
+        d.balance += amount;
+        
+        //UI갱신
+        moneyNum.Refresh();
     }
 
-    public void Withdrawal()
+    public void Withdrawal(int amount)
     {
+        
+        //통장 잔액이 부족하면 팝업에러창을 띄움
+        if(d.balance < amount)
+        {
+            PopupError.SetActive(true);
+            return;
+        }
 
+        d.balance -= amount;
+        d.cash += amount;
+
+        moneyNum.Refresh();
+    }
+
+    public void DepositToInput()
+    {
+        string raw = depositInputField.text;
+
+        int amount;
+
+        if(!int.TryParse(raw, out amount) || amount <= 0)
+        {
+            return;
+        }
+
+        Deposit(amount);
+        depositInputField.text = "";
+    }
+
+    public void WithdrawalToInput()
+    {
+        string raw = withdrawalInputField.text;
+
+        int amount;
+
+        if (!int.TryParse(raw, out amount) || amount <= 0)
+        {
+            return;
+        }
+
+        Withdrawal(amount);
+        withdrawalInputField.text = "";
+    }
+
+    public void ClosePopupError()
+    {
+        PopupError.SetActive(false);
     }
 }
